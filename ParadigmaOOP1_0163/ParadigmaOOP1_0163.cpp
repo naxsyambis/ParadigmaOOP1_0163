2f9439f485762bd9b2410a02bef5c87583443bb4
naxsyambis <naxsyambis@gmail.com>
2024-05-21 10:43:38 +0100

    membuat class untuk objek mahasiswa dan construksi, destrucksi di dalam accses modifier public

--- ParadigmaOOP1_0163/ParadigmaOOP1_0163.cpp
+++ ParadigmaOOP1_0163/ParadigmaOOP1_0163.cpp
@@ -3,7 +3,23 @@
 using namespace std;
 // memberi inililistsasi
 
+class mahasiswa {
+private :
+    const int id;
+    string nama;
+    float nilai;
 
+public :
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
+        //membuat defenisi
+    }
+
+    ~mahasiswa() {
+        cout << "Id   = " << id << endl;
+        cout << "Nama = " << nama << endl;
+        cout << "Nilai = " << nilai << endl;
+    }
+};
 
 int main()
 {
